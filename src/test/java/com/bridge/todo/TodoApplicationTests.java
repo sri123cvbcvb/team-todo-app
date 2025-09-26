4d2b55747a3c2a6fa763b18a2035b2cb61064bdf
sri123cvbcvb <sriramjeeva01@gmail.com>
2025-09-26 11:26:38 +0530

    Initial commit: Spring Boot Todo app

---
+++ src/test/java/com/bridge/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridge.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
